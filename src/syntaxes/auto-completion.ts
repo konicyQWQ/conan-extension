5bbaa7be51488a1961716c73309d48089cf14448
konicyqwq <konicy@outlook.com>
2023-06-07 14:58:56 +0800

    feat: add more keyword completion

--- src/syntaxes/auto-completion.ts
+++ src/syntaxes/auto-completion.ts
@@ -13,6 +13,10 @@ class ConanfileProvider implements vscode.CompletionItemProvider {
                 kind: vscode.CompletionItemKind.Keyword,
                 detail: 'generates all the information needed for CMake to build the packages according to the information passed to Conan about things like the operating system, the compiler to use, architecture, etc. It will also generate cmake-presets files for easy integration with some IDEs that support this CMake feature off-the-shelf.'
             },
+            ...['requires', 'generators', 'tool_requires', 'test_requires', 'options', 'layout'].map(key => ({
+                label: key,
+                kind: vscode.CompletionItemKind.Keyword,
+            })),
         ];
     }
 }
